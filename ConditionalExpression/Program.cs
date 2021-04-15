e99269ecf2b88bb08c003b8ea750fff34d7d9d76
Vik <jzenterprize90@gmail.com>
2021-04-15 15:19:49 -0400

    Conditional expression using a grading system

---
+++ ConditionalExpression/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConditionalExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConditionalExpression();
+        }
+        public static void ConditionalExpression()
+        {
+            Console.WriteLine("Please enter the score");
+            int score = Convert.ToInt32(Console.ReadLine());
+            string gradeLetter = (score <= 55) ? "F" :
+            (score <= 65) ? "D" :
+            (score <= 75) ? "C" :
+            (score <= 85) ? "B" :
+            (score <= 95) ? "A" :
+            "A+";
+            Console.WriteLine(gradeLetter);
+        }
+    }
+}
